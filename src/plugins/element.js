c1215bd43fb8d76134da69923201031d26637606
叶旭 <oldconcept@outlook.com>
2019-11-15 04:14:55 +0800

    完成登录功能

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,4 +1,9 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Form, FormItem, Input, Message } from 'element-ui'
 
 Vue.use(Button)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+// Message的配置需要使用全局挂载，把组件挂载到Vue原型对象上，这样每一个组件都可以通过this.$message来使用Message功能
+Vue.prototype.$message = Message
